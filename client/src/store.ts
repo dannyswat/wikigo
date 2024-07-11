ce5c6eb6d4b8f71d4319a285003c681aa76d4828
Dannys <hinleongwat@gmail.com>
2024-07-11 22:11:35 +0800

    Try redux toolkit

---
+++ client/src/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import authReducer from "./auth/slice";
+
+export default configureStore({
+    reducer: {
+        auth: authReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+})
