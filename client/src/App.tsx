3b277e610d19be8e3b66c89e2bfa6e1a3374ed9e
Dannys <hinleongwat@gmail.com>
2024-07-30 20:56:38 +0800

    Add vscode workspace

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Layout from "./components/Layout";
+import Page from "./pages/Page";
 
 export default function App() {
   return (<BrowserRouter>
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Layout><></></Layout>} />
-      <Route path="/p/:id" element={<Layout><></></Layout>} />
+      <Route path="/p/:id" element={<Layout><Page /></Layout>} />
     </Routes>
   </BrowserRouter>)
 }
